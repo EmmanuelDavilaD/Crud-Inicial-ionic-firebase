ca0bac63c51f6a9223fb2dd53a5beda512b99dbe
desarrollos.chadwick <desarrollos.chadwick@gmail.com>
2019-05-22 10:43:00 +0200

    esto es un crud inicial para proximos proyectos que ya incluye , registro login , confirmar email , roles , olvide contraseñana , tabs

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDWcNj8_2WDwb3bWb4XocyrzthTznWPXQM",
+    authDomain: "rentechclimbs-7b317.firebaseapp.com",
+    databaseURL: "https://rentechclimbs-7b317.firebaseio.com",
+    projectId: "rentechclimbs-7b317",
+    storageBucket: "rentechclimbs-7b317.appspot.com",
+    messagingSenderId: "468450553101",
+    appId: "1:468450553101:web:539bacbd1ce2e0ac"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
